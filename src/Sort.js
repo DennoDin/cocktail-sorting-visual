09b5709ff515f608ce36ea5c25ccb86018fc85ce
Dustin <dustin.tran@live.com>
2018-07-18 18:47:38 +0900

    completed compare and sort functions

--- src/Sort.js
+++ src/Sort.js
@@ -1,11 +1,47 @@
-class SomeKindOfSort {
+class CocktailSort {
   constructor(array) {
-    this.array = array;
+    this.array = [];
+    for (let idx = 0; idx < array.length; idx++) {
+      this.array.push(array[idx]);
+    }
+    return this;
   }
   sort() {
-    console.log("sort!");
-    console.log("sort!");
-    console.log("sort!");
+    let rightLimit = this.array.length;
+    let rightItr = 0;
+    let leftItr = 0;
+    let leftLimit = 0;
+    while (leftLimit <= rightLimit) {
+      let count = 0;
+      for (rightItr; rightItr < rightLimit; rightItr++) {
+        if (this.compare(this.array[rightItr], this.array[rightItr + 1])) {
+          let temp = this.array[rightItr];
+          this.array[rightItr] = this.array[rightItr + 1];
+          this.array[rightItr + 1] = temp;
+          count++;
+        }
+      }
+      rightLimit = rightItr - 1;
+      leftItr = rightLimit;
+      for (leftItr = rightLimit; leftItr > leftLimit; leftItr = leftItr - 1) {
+        if (this.compare(this.array[leftItr - 1], this.array[leftItr])) {
+          let temp = this.array[leftItr];
+          this.array[leftItr] = this.array[leftItr - 1];
+          this.array[leftItr - 1] = temp;
+          count++;
+        }
+      }
+      leftLimit = leftItr + 1;
+      rightItr = leftLimit;
+      if (count == 0) {
+        leftLimit = rightLimit + 1;
+      }
+    }
+  }
+
+  compare(left, right) {
+    // checks if the the left side bigger
+    return left > right;
   }
 
   returnValue(value) {
@@ -13,4 +49,4 @@ class SomeKindOfSort {
   }
 }
 
-module.exports = SomeKindOfSort;
+module.exports = CocktailSort;
