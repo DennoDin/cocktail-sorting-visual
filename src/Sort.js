08f164e2d75aa8a69580a76a3862de0e83641564
Dustin <dustin.tran@live.com>
2018-07-19 17:09:19 +0900

    created new stepSort function which performs one step of sorting per call

--- src/Sort.js
+++ src/Sort.js
@@ -4,10 +4,61 @@ class CocktailSort {
     for (let idx = 0; idx < array.length; idx++) {
       this.array.push(array[idx]);
     }
+    this.sortRight = true;
+    this.rightItr = 0;
+    this.rightLimit = this.array.length - 1;
+    this.leftItr = this.rightLimit;
+    this.leftLimit = 0;
+
     return this;
   }
+
+  // RETURN PER STEP SORT
+
+  stepSort() {
+    if (this.leftLimit < this.rightLimit) {
+      if (this.sortRight) {
+        if (this.rightItr < this.rightLimit) {
+          if (
+            this.compare(
+              this.array[this.rightItr],
+              this.array[this.rightItr + 1]
+            )
+          ) {
+            let temp = this.array[this.rightItr];
+            this.array[this.rightItr] = this.array[this.rightItr + 1];
+            this.array[this.rightItr + 1] = temp;
+          }
+        } else {
+          this.rightLimit = this.rightItr - 1;
+          this.leftItr = this.rightLimit;
+          this.sortRight = false;
+        }
+        this.rightItr++;
+      } else {
+        if (this.leftItr > this.leftLimit) {
+          if (
+            this.compare(this.array[this.leftItr - 1], this.array[this.leftItr])
+          ) {
+            let temp = this.array[this.leftItr];
+            this.array[this.leftItr] = this.array[this.leftItr - 1];
+            this.array[this.leftItr - 1] = temp;
+          }
+        } else {
+          this.leftLimit = this.leftItr + 1;
+          this.rightItr = this.leftLimit;
+          this.sortRight = true;
+        }
+        this.leftItr = this.leftItr - 1;
+      }
+    }
+    return this.array;
+  }
+
+  // ONE RETURN COMPLETE SORT
+
   sort() {
-    let rightLimit = this.array.length;
+    let rightLimit = this.array.length - 1;
     let rightItr = 0;
     let leftItr = 0;
     let leftLimit = 0;
